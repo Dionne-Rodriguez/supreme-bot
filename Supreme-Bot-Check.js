fbfb3f6ac5f8c289e472bcb118578c091059ab5d
Dionne Rodriguez <Dionne@MacBook-Pro.local>
2020-10-21 14:22:37 -0400

    WIP - page refresh behvaior for new products

--- Supreme-Bot-Check.js
+++ Supreme-Bot-Check.js
@@ -17,41 +17,90 @@ scraperProduct("https://www.supremenewyork.com/shop")
 async function scraperProduct(url) {
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 250
+        slowMo: 75
     });
     const page = await browser.newPage();
     await page.goto(url)
 
-    const getSupremeLinks = await page.evaluate(() => {
 
-        let data = [];
+    //refresh the page until the new tags are not on the previous stored new tags
+    //when the new tags don't match with the old new tags stop the refresh
+    //grab the first 5 product links
+    //procede with the function
+   
+
+    const getSupremeLinks = await page.evaluate(() => {
+        let newSupremeProductLinks = [];
+        let OldSupremeProductLinks = ['https://www.supremenewyork.com/shop/shoes/tfwm0d3tb',
+        'https://www.supremenewyork.com/shop/hats/dmbz4ytfx',
+        'https://www.supremenewyork.com/shop/jackets/rfc1ir8zo',
+        'https://www.supremenewyork.com/shop/jackets/dyupjinrd',
+        'https://www.supremenewyork.com/shop/jackets/owi1hbg04',
+        'https://www.supremenewyork.com/shop/shirts/qtpkj86ma',
+        'https://www.supremenewyork.com/shop/shirts/ov7feyd38',
+        'https://www.supremenewyork.com/shop/sweatshirts/jxzscypmb',
+        'https://www.supremenewyork.com/shop/sweatshirts/ey2bz0fg7',
+        'https://www.supremenewyork.com/shop/sweatshirts/xljctrkau',
+        'https://www.supremenewyork.com/shop/tops-sweaters/qpcy4t89v',
+        'https://www.supremenewyork.com/shop/tops-sweaters/uk1fsbq4n',
+        'https://www.supremenewyork.com/shop/pants/q1i3depsc',
+        'https://www.supremenewyork.com/shop/pants/gspaid2rv',
+        'https://www.supremenewyork.com/shop/pants/j12wldagx',
+        'https://www.supremenewyork.com/shop/hats/gnyemz2tb',
+        'https://www.supremenewyork.com/shop/hats/mdps36b02',
+        'https://www.supremenewyork.com/shop/hats/v4imdyu2r',
+        'https://www.supremenewyork.com/shop/hats/jbnuwprdx',
+        'https://www.supremenewyork.com/shop/accessories/t435zxwsi',
+        'https://www.supremenewyork.com/shop/skate/whutpe1wr',
+        'https://www.supremenewyork.com/shop/skate/bump7gjf4']
         let ul = document.querySelector('#shop-scroller');
         let li = ul.querySelectorAll('li')
 
-        li.forEach(el => {
-            // if (el.childNodes[0].childNodes[0].attributes[0].nodeValue == "new_item_tag") {
-                data.push(el.childNodes[0].href)
-            // }
+        li.forEach(el => { 
+             if (el.childNodes[0].childNodes[0].attributes[0].nodeValue == "new_item_tag") {
+                 console.log("CHECKING");
+                 
+                newSupremeProductLinks.push(el.childNodes[0].href)
+             }
         });
-        return data
+        
+        const allEqual = arr => arr.every( v => v === arr[0] )
+        allEqual( [1,1,1,1] )
+        // if(newSupremeProductLinks === letOldSupremeProductLinks) {
+        //     console.log("no new products found");
+        //     page.reload({ waitUntil: 'networkidle0' })
+             
+        // }
+        //  else {
+        //      console.log("found new Products");
+             
+        //     return newSupremeProductLinks
+        // }
+
+        return "no new products found"
     })
         .catch(() => console.log("error"));
 
-    getAvailableSupremeProducts(page,getSupremeLinks);
+        console.log("NEW",getSupremeLinks)
 
-    console.log(getSupremeLinks)
+    //getAvailableSupremeProducts(page,getSupremeLinks);
+
+    
 
 
     // await browser.close();
 }
 const getAvailableSupremeProducts = async (page, links) => {
-    links = [
-        'https://www.supremenewyork.com/shop/jackets/xni1oh9ca',
-        'https://www.supremenewyork.com/shop/jackets/zvj0wbe51',
-      ]
-    for (let i = 0; i < links.length; i++) {
+    // links = [
+    //     'https://www.supremenewyork.com/shop/jackets/xni1oh9ca',
+    //     'https://www.supremenewyork.com/shop/jackets/zvj0wbe51',
+    //   ]
+    let reducedAmountsOfLinks = links.slice(0,5)
+    console.log("SLICED LINKS", reducedAmountsOfLinks);
+    
+    for (let i = 0; i < reducedAmountsOfLinks.length; i++) {
         page.goto(links[i])
-        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
         //evaluates the page context
         if (await page.$("input[name=commit]") !== null) {
@@ -63,14 +112,15 @@ const getAvailableSupremeProducts = async (page, links) => {
            // page.click("input[name=commit]")
 
         }
+
         
     }
-
+console.log("HEREEE");
 
  await page.click('a[data-no-turbolink]'); 
-enterPaymentInfo(page)
 
-    
+
+ enterPaymentInfo(page)
     return null
 
 }
